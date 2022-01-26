2ab23aead3aa921028df1dcb34e8e2a6d4d8fbbd
Rick <rickatherton316@gmail.com>
2022-01-26 17:01:51 +0000

    Add timer functionality to move the Power Station around the Grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const squares = document.getElementsByClassName('square')
-const powerStation = document.getElementsByClassName('power-station')
-const timeLeft = document.getElementById('time-left')
-const score = document.getElementById('score')
+let squares = document.getElementsByClassName('square')
+let powerStation = document.getElementsByClassName('power-station')
+let timeLeft = document.getElementById('time-left')
+let score = document.getElementById('score')
 
 let result = 0
 
@@ -12,7 +12,15 @@ function randomSquare() {
 
     let randomSquare = squares[Math.floor(Math.random() * 9)];
     randomSquare.classList.add('powerStation')
-
 }
 
-randomSquare()
+// Add button to start the game here
+
+function movePowerStation() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+}
+
+movePowerStation()
+
+
