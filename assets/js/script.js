8064f550e2f5f13642509f58a12e6c4125a6f8aa
Rick <rickatherton316@gmail.com>
2022-01-27 12:30:11 +0000

    Add countdown timer functionality including setting the interval to 1000 secs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-let squares = document.getElementsByClassName('square');
-let powerStation = document.getElementsByClassName('power-station');
-let timeLeft = document.getElementById('time-left');
-let score = document.getElementById('score');
+const squares = document.getElementsByClassName('square');
+const powerStation = document.getElementsByClassName('power-station');
+const timeLeft = document.getElementById('time-left');
+const score = document.getElementById('score');
 
 let result = 0
 let hitPosition
+let currentTime = 60
+let timerId = null
 
 function randomSquare() {
     for (let i = 0; i <= squares.length; i++) {
@@ -18,7 +20,7 @@ function randomSquare() {
 }
 
 squares.forEach(square => {
-    square.addEventListener('mousedown' () => {
+    square.addEventListener('onmousedown' () => {
         if (square.id == ) {
             result++
             score.innerHTML = result
@@ -34,6 +36,21 @@ function movePowerStation() {
 }
 
 movePowerStation()
+
+// Add alert to game over card
+
+function countDown () {
+    currentTime--
+    timeLeft.innerHTML = currentTime
+
+    if(currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('You did it! By stopping {$score} Power Stations burning fossil fuels, Earth is saved for another billion years. Remember to promote Renewable Energy and Go Green in everything you do!')
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
 
 
 
