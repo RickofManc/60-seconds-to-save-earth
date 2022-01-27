94c4ac72edf73c339c03955210eb5884fa4b26b2
Rick <rickatherton316@gmail.com>
2022-01-27 16:02:00 +0000

    Fix bug with JavaScript code that prevents randomsquare functionality working as the forEach loop was not working correctly. Change selectros from getElementsByClassName to querySelectorAll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,59 @@
-const squares = document.querySelector('.square')
-const powerStation = document.querySelector('.power-station')
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+const squares = document.querySelectorAll('.square');
+const coal = document.querySelector('.coal');
+const timeLeft = document.querySelector('#time-left');
+const score = document.querySelector('#score');
 
-let result = 0
-let hitPosition
-let currentTime = 60
-let timerId = null
+let result = 0;
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
+
+// Game function to pop-up the coal image on random grid squares
 
 function randomSquare() {
-    for (let i = 0; i <= squares.length; i++) {
-        square.classList.remove('powerStation')
-    }
+    squares.forEach(square => {
+        square.classList.remove('coal');
+    });
 
-    let randomSquare = squares[Math.floor(Math.random() * 9)]
-    randomSquare.classList.add('powerStation')
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('coal');
 
-    hitPosition = randomSquare.id
+    hitPosition = randomSquare.id;
 }
 
-for (let i = 0; i <= squares.length; i++) {
+squares.forEach(square => {
     square.addEventListener('mousedown', () => {
         if (square.id == hitPosition) {
-            result++
-            score.innerHTML = result
-            hitPosition = null
+            result++;
+            score.textContent = result;
+            hitPosition = null;
         }
-    })
-}
+    });
+});
+
 
 // Add timerID to button to start the game here
 
-function movePowerStation() {
-    timerId = setInterval(randomSquare, 500)
+function moveCoal() {
+    timerId = setInterval(randomSquare, 700);
 }
 
-movePowerStation()
+moveCoal();
 
 // Add alert to game over card
 
 function countDown() {
-    currentTime--
-    timeLeft.innerHTML = currentTime
+    currentTime--;
+    timeLeft.textContent = currentTime;
 
     if (currentTime == 0) {
-        clearInterval(countDownTimerId)
-        clearInterval(timerId)
-        alert('You did it! By stopping {$result} Power Stations burning fossil fuels, Earth is saved for another billion years. Remember to promote Renewable Energy and Go Green in everything you do!')
+        clearInterval(countDownTimerId);
+        clearInterval(timerId);
+        alert('You did it! By stopping {$result} Power Stations burning fossil fuels, Earth is saved for another billion years. Remember to promote Renewable Energy and Go Green in everything you do!');
     }
 }
 
-let countDownTimerId = setInterval(countDown, 1000)
+let countDownTimerId = setInterval(countDown, 1000);
 
 
 
